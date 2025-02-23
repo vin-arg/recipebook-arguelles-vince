e4d1b18927acc4044d401086bf5f64676e8d53fd
vin-arg <152352398+vin-arg@users.noreply.github.com>
2025-02-23 15:14:03 +0800

    made the code for urls.py and views.py in the app folder and created 3 html files inside the templates folder

---
+++ recipebook/ledger/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import recipes, recipe_ingredients
+
+urlpatterns = [
+    path('recipes/list/', recipes, name="recipes"),
+    path('recipe/<int:num>/', recipe_ingredients, name="recipe_ingredients")
+
+]
