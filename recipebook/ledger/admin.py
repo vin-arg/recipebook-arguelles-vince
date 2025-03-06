95f150392967f69dda3278ce987f4dc72c602f43
vin-arg <152352398+vin-arg@users.noreply.github.com>
2025-03-06 23:43:25 +0800

    made the code for models.py and admin.py

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Ingredient, Recipe, RecipeIngredient
 
-# Register your models here.
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    list_display = ('quantity', 'ingredient', 'recipe',)
+
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
