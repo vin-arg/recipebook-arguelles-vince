49c58506aa02b870e1dfd2f9b699a55cc0ecfec9
vin-arg <152352398+vin-arg@users.noreply.github.com>
2025-03-20 13:13:00 +0800

    created registration folder and login.html, added code to login, recipe_ingredients, recipe_list, admin, models, views, and project level urls.

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from .models import Recipe
+from django.contrib.auth.decorators import login_required
+
 
 def recipes(request):
     recipes_list = Recipe.objects.all()
     return render(request, 'recipe_list.html', {'recipes': recipes_list})
 
+@login_required
 def recipe_ingredients(request, num):
     recipe = Recipe.objects.filter(id=num).first()
     ingredients = recipe.ingredients.all() if recipe else []
-    return render(request, 'recipe_ingredients.html', {'recipe': recipe, 'ingredients': ingredients})
+    author_name = recipe.author.username if recipe else "Unknown"
+    return render(request, 'recipe_ingredients.html', {'recipe': recipe, 'ingredients': ingredients, 'author_name': author_name})
